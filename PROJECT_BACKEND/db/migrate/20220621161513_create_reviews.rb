b528950e1073bcf67a2692bd491482ee18b11216
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-06-22 23:31:25 -0400

    Update 20220621161513_create_reviews.rb

--- PROJECT_BACKEND/db/migrate/20220621161513_create_reviews.rb
+++ PROJECT_BACKEND/db/migrate/20220621161513_create_reviews.rb
@@ -1,6 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.belongs_to :tours, foreign_key: true
       t.string :username
       t.string :review
       t.integer :rating
