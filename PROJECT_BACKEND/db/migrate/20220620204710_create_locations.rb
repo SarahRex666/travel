b9a6ed786da2b3a223f97dd63825c0594a3ab1d5
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-06-22 23:31:52 -0400

    Update 20220620204710_create_locations.rb

--- PROJECT_BACKEND/db/migrate/20220620204710_create_locations.rb
+++ PROJECT_BACKEND/db/migrate/20220620204710_create_locations.rb
@@ -1,10 +1,10 @@
 class CreateLocations < ActiveRecord::Migration[6.1]
   def change
     create_table :locations do |t|
+      t.belongs_to :tour, foreign_key: true
+      t.belongs_to :guide, foreign_key: true
       t.string :name
       t.string :description
-      t.integer :guide_id
-      t.integer :tour_id
     end
   end
 end
