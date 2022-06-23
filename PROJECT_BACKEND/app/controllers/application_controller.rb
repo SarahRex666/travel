aa54e642f989ec7781705f89df2d29248b21e1c2
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-06-22 23:34:00 -0400

    Update application_controller.rb

--- PROJECT_BACKEND/app/controllers/application_controller.rb
+++ PROJECT_BACKEND/app/controllers/application_controller.rb
@@ -1,10 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  # get "/tours" do
-  #   tours = Tours.all
-  #   tours.to_json
-  # end
 
+  get '/tours' do
+    tours = Tour.all
+    tours.to_json
+  end
+
+  get '/guides' do
+    guides = Guide.all
+    guides.to_json
+  end
+
+  get '/addons' do
+    addons = Addon.all
+    addons.to_json
+  end
+  
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get '/locations' do
+    locations = Location.all
+    locations.to_json
+  end
+  
 end
